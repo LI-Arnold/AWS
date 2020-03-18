60d20b5e35bd29c46c77c46203eb18aeb0fc9d46
Utilisateur <user@debian.arcahe.ovh>
2020-03-18 18:45:10 +0100

    formulaire to bdd

--- enregistrer.js
+++ enregistrer.js
@@ -1,20 +1,47 @@
-var form = document.querySelector("form");
-form.addEventListener("submit", function(e) {
-  var nom = form.elements.nom.value;
-  var prenom = form.elements.prenom.value;
-  var pseudo = form.elements.pseudo.value;
-  var email = form.elements.email.value;
-  var mdp1 = form.elements.mdp1.value;
-  var mdp2 = form.elements.mdp2.value;
-  if ( mdp1 === mdp2){
-  console.log(
-    "Vous vous appelé" + nom + prenom + "votre pseudo est " + pseudo +  "le mot de passe" + mdp + "votre email "+ email + " repeatemail = "  + repeatemail
-  );
-}
-else {
-        message = "Erreur : les mots de passe saisis sont différents";
-    }
-  document.getElementById("infoMdp").textContent = message;
-  e.preventDefault();
+var express = require('express');
+var app = express();
+var  bodyParser = require('body-parser');
+var mysql = require('mysql');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "user",
+  password: "user",
+  database: "tron"
 });
+
+
+app.get('/',function(req,res){
+	res.render('enregistrer.ejs');
+});
+
+app.post('/',function(req,res){
+	console.log('coizejo');
+	con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+	 var sql = "INSERT INTO `joueur` (`psudo`,`nom`,`prenom`,`email`,`mot_pass`) VALUES ('" + req.body.pseudo + "','" + req.body.nom + "', '" + req.body.prenom + "','" + req.body.email + "', '" + req.body.mdp1 + "')";
+    con.query(sql,function(error) {
+        if (error) { 
+            console.log(error.message);
+        } else {
+            console.log('success');   
+        }
+    });
+    res.render('enregistrer.ejs', {title: 'Express'});
+});
+	
+//~ app.use(function(req, res, next){
+    //~ res.setHeader('Content-Type', 'text/plain');
+    //~ res.status(404).send('Page introuvable !');
+//~ });
+
+app.listen(3000,function(){
+	console.log('Ecoute sur le port 3000');
+});
+
+
 
